e840a726c3abf956d07047db952db33fec6717e1
HamSik <payback9488@gmail.com>
2024-11-25 14:51:46 +0900

    [add]: NameSpace.swift & KioskViewController.PreviewProvider

---
+++ NBC-W6-Kiosk/Controller/KioskViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  NBC-W6-Kiosk
+//
+//  Created by 황석현 on 11/25/24.
+//
+
+import UIKit
+import SwiftUI
+
+class KioskViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
+struct KioskViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        KioskViewController_Presentable()
+    }
+    
+    struct KioskViewController_Presentable: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> some UIViewController {
+            KioskViewController()
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        }
+    }
+}
---
+++ NBC-W6-Kiosk/Utility/NameSpace.swift
@@ -0,0 +1,7 @@
+//
+//  NameSpace.swift
+//  NBC-W6-Kiosk
+//
+//  Created by 황석현 on 11/25/24.
+//
+
