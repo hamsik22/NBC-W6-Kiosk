b55f6122861f4065275007ef9b0ae2b603f2bd32
HwangSukHyeon <payback9488@gmail.com>
2024-11-27 17:02:52 +0900

    [Add]: Set OrderListView Code & Proj Setting
    - OrderListView의 기초 코드를 구현했습니다.
    - SnapKit.dynamic을 제거하였습니다.

--- NBC-W6-Kiosk/View/OrderList.swift
+++ NBC-W6-Kiosk/View/OrderList.swift
@@ -10,26 +10,71 @@ import UIKit
 
 class OrderListView: UIView {
     
-    var itemList: [MenuItem] = []
-
-    // 주문내역이 없을 시 표시되는 문구
-    let noOrderText = UILabel()
-    // 주문내역을 표시하는 테이블 뷰
-    let orderList = UITableView()
-    // 주문수량, 주문금액을 표시하는 뷰
-    let totalOrderText = UIStackView() // horizontal
+    var itemList: [DefaultProduct] = []
     
-    // 백그라운드 색 넣자
+    // 주문내역이 없을 시 표시되는 문구
+    private let noOrderText = UILabel()
+    // 주문내역을 표시하는 테이블 뷰
+    private let orderList = UITableView()
+    // 주문수량, 주문금액을 표시하는 뷰
+    private let totalOrderText = UIStackView() // horizontal
+    
     func setupOrderListView() {
+        noOrderText.text = "메뉴를 선택해 주세요"
+        noOrderText.textAlignment = .center
+        noOrderText.translatesAutoresizingMaskIntoConstraints = false
+        
+        //TODO: cell 등록 필요
+        orderList.delegate = self
+        orderList.dataSource = self
+        orderList.translatesAutoresizingMaskIntoConstraints = false
+        
+        setupTotalOrderText()
+        
+        [noOrderText, orderList, totalOrderText]
+            .forEach{ addSubview($0) }
+        
+        // TODO: 레이아웃 설정
+        NSLayoutConstraint.activate([
+            // noOrderText의 위치
+            noOrderText.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            noOrderText.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            noOrderText.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            
+            // orderList의 위치
+            orderList.topAnchor.constraint(equalTo: noOrderText.bottomAnchor, constant: 20),
+            orderList.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            orderList.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            orderList.bottomAnchor.constraint(equalTo: totalOrderText.topAnchor, constant: -20),
+            
+            // totalOrderText의 위치
+            totalOrderText.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            totalOrderText.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            totalOrderText.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
+            totalOrderText.heightAnchor.constraint(equalToConstant: 50)
+        ])
         
     }
-}
-
-class OrderList: UITableView {
-    let orderList = UITableView()
     
-    func setUpOrderList() {
+    func setupTotalOrderText() {
         
+        let amountLabel = UILabel()
+        amountLabel.text = "총 0/20개"
+        amountLabel.textAlignment = .right
+        amountLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let priceLabel = UILabel()
+        priceLabel.text = "0원"
+        priceLabel.textAlignment = .right
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        totalOrderText.axis = .horizontal
+        totalOrderText.distribution = .equalSpacing
+        totalOrderText.alignment = .trailing
+        totalOrderText.translatesAutoresizingMaskIntoConstraints = false
+        
+        [amountLabel, priceLabel]
+            .forEach{ totalOrderText.addSubview($0) }
     }
 }
 
