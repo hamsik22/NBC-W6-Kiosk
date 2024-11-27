5d6c77fe52cb769ea7535763c14080d929041463
Jeon_Seong_gyu <junsung3807@naver.com>
2024-11-27 11:29:42 +0900

    Fix: Refact Product model
    - View에서 사용할 properties 추가
    - Subject 프로토콜 채택 및 구현

--- NBC-W6-Kiosk/Model/Product.swift
+++ NBC-W6-Kiosk/Model/Product.swift
@@ -7,18 +7,40 @@
 
 import Foundation
 
-struct Product {
-    private let name: String
-    private let price: Int
-    private let thumbnailImageString: String
-    private var stock: Int
-    private var selectedCount: Int
+protocol Product: Subject {
+    /// selectedCount 프로퍼티 값이 변경됐을 때 Observer에 알리는 함수
+    /// - Parameter selectedCount: Observer 함수의 매개변수로 전달
+    func notify(selectedCount: Int)
+}
+
+struct DefaultProduct: Product {
+    private var observers: [Observer] = []
     
-    init(name: String, price: Int, thumbnailImageString: String, stock: Int, selectedCount: Int = 0) {
+    let category: Category
+    let name: String
+    let englishName: String
+    let price: Int
+    let thumbnailImageString: String
+    
+    /// if stock == 0 { sould out }
+    var stock: Int
+    private var selectedCount: Int {
+        didSet { notify(selectedCount: selectedCount) }
+    }
+    
+    init(category: Category, name: String, englishName: String, price: Int, thumbnailImageString: String, stock: Int, selectedCount: Int = 0) {
+        self.category = category
         self.name = name
+        self.englishName = englishName
         self.price = price
         self.thumbnailImageString = thumbnailImageString
         self.stock = stock
         self.selectedCount = selectedCount
+    }
+    
+    mutating func addObserver(_ observer: any Observer) { observers.append(observer) }
+    
+    func notify(selectedCount: Int) {
+        // TODO: 장바구니 화면 연동까지 확인 후 구연
     }
 }
